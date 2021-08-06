8c7463b7b8de938cb4b7e22d71655a29fbadf956
Nina Vasilieva <aninok92@gmail.com>
2021-08-06 15:34:08 +0300

    добавляет сохрание данных в local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,42 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import logger from "redux-logger";
 import contactsReducer from "./contacts/contacts-reducer";
 
-const middleware = [...getDefaultMiddleware(), logger];
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+  logger,
+];
+
+const contactsPersistConfig = {
+  key: "contacts",
+  storage,
+  blacklist: ["filter"],
+};
 
 const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistReducer(contactsPersistConfig, contactsReducer),
   },
   middleware,
   devTools: process.env.NODE_ENV !== "production",
 });
 
-console.log(getDefaultMiddleware());
+const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
